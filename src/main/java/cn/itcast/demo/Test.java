1c498fe502bfd5bbba641db38a9f3b55bdf92957
chl <chenhuali@itcast.cn>
2018-07-23 16:20:47 +0800

    1

--- src/main/java/cn/itcast/demo/Test.java
+++ src/main/java/cn/itcast/demo/Test.java
@@ -8,12 +8,12 @@ package cn.itcast.demo;
  */
 public class Test {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("========1111========");
-		System.out.println("========2222========");
-		System.out.println("=========333333===========");
+        System.out.println("========1111========");
+        System.out.println("========2222========");
+        System.out.println("=========333333===========");
         System.out.println();
-	}
+    }
 
 }
